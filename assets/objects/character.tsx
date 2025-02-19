3efc188cbfd9e7ffd188cd0392e1a48e84cfecae
Sam Hapke <gamidoi@gmail.com>
2025-02-19 12:20:04 -0600

    object Characters have a 'characterLevel' block, and because of implementation, I also added 'fullCasterLevels', 'halfCasterLevels', and 'warlockLevels'. proficiency is now tied to characterLevel, and SavesRolls page updates accordingly. while updating, SavesRolls page, added core stat specific background colors to the skills, for readability. ---- this branch has exeeded its purpose. last clean up before closing will be to implement warlock spells on the 'Character' tab.

--- assets/objects/character.tsx
+++ assets/objects/character.tsx
@@ -3,7 +3,12 @@ export class Character {
     charName: string;
     maxHP: number;
     spellcastingLevel: number;
+    characterLevel: number;
+    fullCasterLevel: number;
+    halfCasterLevel: number;
+    warlockCasterLevel: number;
     currentUsedSpells: string;
+    warlockCurrentUsedSpells: string;
     abilities: {};
     spells: {};
 
@@ -35,26 +40,42 @@ export class Character {
     persuasion: string
 
 
-    constructor(character: string, HPMax = 10, castingLevel = 0) {
+    constructor(character: string, HPMax = 10, characterLevel = 0) {
         this.charName = character;
         this.maxHP = HPMax;
-        this.spellcastingLevel = castingLevel;
+        this.spellcastingLevel = 0;
+        this.characterLevel = characterLevel;
+        this.fullCasterLevel = this.warlockCasterLevel = this.halfCasterLevel = 0;
         this.currentUsedSpells = "0000000000000000000000"
+        this.warlockCurrentUsedSpells = "0000"
         this.abilities = {};
         this.spells = {};
         this.STR = this.DEX = this.CON = this.INT = this.WIS = this.CHA = 10;
-        this.proficiency = 2;
+        this.proficiency = this.updateProficiency(characterLevel);
 
         this.athletics = this.acrobatics = this.sleightOfHand = this.stealth = this.arcana = this.history = this.investigation = this.nature = this.religion =
             this.animalHandling = this.insight = this.medicine = this.perception = this.survival = this.deception = this.intimidation = this.performance =
                 this.persuasion = "X";
     }
 
-    public getSkills() {
-
-
-
-
-        return  "stringy string";
+    public updateProficiency(characterLevel :number) :number {
+        this.proficiency = Math.ceil((4+characterLevel)/4);
+        return  this.proficiency;
     }
+    public updateSpellcastingLevel(fullCasterLevel :number, halfCasterLevel :number) :number {
+        this.fullCasterLevel = fullCasterLevel;
+        this.halfCasterLevel = halfCasterLevel;
+        this.spellcastingLevel = Math.floor(fullCasterLevel + (halfCasterLevel / 2));
+        return  this.spellcastingLevel;
+    }
+}
+export function updateProficiency(thisCharacter :Character, characterLevel :number) :number {
+    thisCharacter.proficiency = Math.ceil((4+characterLevel)/4);
+    return  thisCharacter.proficiency;
+}
+export function updateSpellcastingLevel(thisCharacter :Character, fullCasterLevel :number, halfCasterLevel :number) :number {
+    thisCharacter.fullCasterLevel = fullCasterLevel;
+    thisCharacter.halfCasterLevel = halfCasterLevel;
+    thisCharacter.spellcastingLevel = Math.floor(fullCasterLevel + (halfCasterLevel / 2));
+    return  thisCharacter.spellcastingLevel;
 }
